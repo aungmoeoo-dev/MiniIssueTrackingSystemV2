3a153d20eb8b584d37476c12a27b2e7df19a5f6a
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-12-23 21:15:43 +0630

    fix: add Id to UserModel & CommentModel

--- MiniIssueTrackingSystemV2.Database/Models/CommentModel.cs
+++ MiniIssueTrackingSystemV2.Database/Models/CommentModel.cs
@@ -8,6 +8,7 @@ namespace MiniIssueTrackingSystemV2.Database.Models;
 
 public class CommentModel
 {
+	public string Id { get; set; }
 	public string Text { get; set; }
 	public string CreatedBy { get; set; }
 }
--- MiniIssueTrackingSystemV2.Database/Models/UserModel.cs
+++ MiniIssueTrackingSystemV2.Database/Models/UserModel.cs
@@ -8,6 +8,7 @@ namespace MiniIssueTrackingSystemV2.Database.Models;
 
 public class UserModel
 {
+	public string Id { get; set; }
 	public string Username { get; set; }
 	public string Email { get; set; }
 }
